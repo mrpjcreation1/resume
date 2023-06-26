90098af6bfdbf8e2dfff5bcf4e7a8ef42eca4872
mrpjcreation1 <132059274+mrpjcreation1@users.noreply.github.com>
2023-06-25 23:10:58 -0700

    index.php

---
+++ index.php
@@ -0,0 +1,38 @@
+
+<?php 
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Admition</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css">
+  <script src="https://cdn.jsdelivr.net/npm/jquery@3.6.4/dist/jquery.slim.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js"></script>
+</head>
+<body>
+
+<div class="container">
+  <form method="post" action="action_page.php">
+    <div class="form-group">
+      <label>name:</label>
+      <input  class="form-control" placeholder="name" name="name">
+    </div>
+    <div class="form-group">
+      <label >class:</label>
+      <input class="form-control" placeholder="phone" name="class">
+    </div>
+    <div class="form-group">
+        <label >age:</label>
+        <input class="form-control" placeholder="gender" name="age">
+      </div>
+    <button class="btn btn-primary" name="submit">Submit</button>
+  </form>
+</div>
+
+</body>
+</html>
+
